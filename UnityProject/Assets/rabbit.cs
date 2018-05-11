164f00905f7eff9ad6db4c4b7b6597bf94eeb219
przemekrogala <36774383+przemekrogala@users.noreply.github.com>
2018-05-11 23:10:26 +0200

    Dodałem króliki i wiecej wilków
    Dodałem i oprogramowalem króliki, uciekaja gdy gracz sie zbliza, a jak sie oddali to nie uciekaja.

---
+++ UnityProject/Assets/rabbit.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rabbit : Character
+{
+    private Transform target;
+    public int moveSpeed;
+    public int rotationSpeed;
+
+
+    public Transform Target
+    {
+        get
+        {
+            return target;
+        }
+
+
+        set
+        {
+
+
+            target = value;
+        }
+
+
+
+    }
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FollowTarget();
+
+    }
+
+
+    private void FollowTarget()
+    {
+
+        Animator animator = GetComponent<Animator>();
+        if (target != null)
+        {
+
+            animator.SetFloat("x", -direction.x);
+            animator.SetFloat("y", direction.y);
+            direction =(target.transform.position - transform.position).normalized;
+            transform.position =Vector2.MoveTowards(transform.position, target.position, -1 * Time.deltaTime);
+
+        }
+        else
+
+            direction = Vector2.zero;
+
+
+
+
+
+    }
+
+}
---
+++ UnityProject/Assets/range.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class range : MonoBehaviour
+{
+
+
+    private rabbit parent;
+    // Use this for initialization
+    void Start()
+    {
+        parent = GetComponentInParent<rabbit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+
+
+            parent.Target = collision.transform;
+
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+
+    {
+
+        if (collision.tag == "Player")
+        {
+
+            parent.Target = null;
+        }
+
+    }
+
+
+
+}
+
+
